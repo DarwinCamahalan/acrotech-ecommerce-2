675a50d90e0b5f81dc4755dc53aeff7ce18519cb
kingchanchaan <neilaragones@gmail.com>
2021-05-02 16:37:03 +0800

    Frontend

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -2,7 +2,19 @@ import './styles.scss'
 const AboutPage = () => {
   return (
     <div className="about-page">
-      <p>About Page - Being Built 🚧</p>
+      <div class="row">
+        <div class="col-md-4 text-center">
+          <div class="profile">
+            <img src="" class="user"></img>
+            <blockquote>Lggmrkmgrkgmrkgmdcmdkcckdmcd
+              cddcdmcdkcmd.</blockquote>
+              <h4>John R. Cruz</h4>
+              <h5>Co-Founder at ACRO TECH</h5>
+          </div>
+        </div> 
+
+
+      </div> 
     </div>
   )
 }
