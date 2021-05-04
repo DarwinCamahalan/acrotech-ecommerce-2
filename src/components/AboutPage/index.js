1a8b773573b1c522a80bbc2cb9f21c8901097749
kingchanchaan <neilaragones@gmail.com>
2021-05-04 20:20:33 +0800

    Niel Code

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -1,42 +1,72 @@
 import './styles.scss'
+import ClientImage from '../../assets/test.jpg'
 const AboutPage = () => {
   return (
-    <div id="testimonials">
-      <div class="container"> 
-        <div class="owl-carousel owl-theme">
-          <div class="testimonials-box item">
-            <div class="testimonial-photo">
-              <img src="/assets/test.jpg" alt="Client Photo"></img>
+    <div className="testimonials">
+      <div className="content">
+        {/* TITLE OF THE CARDS */} 
+        <div className="title">
+          <h1>Testimonials</h1>
+          <hr />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea,
+            aliquam.
+          </p>
+        </div>
+
+        <div className="container">
+          {/* THE CARD ITSELF */}
+          <div className="card">
+            {/* CLIENT IMAGE */}
+            <div className="client-image">
+              <img src={ClientImage} alt="Client Photo" />
             </div>
-            <h5>Jericho Cruz</h5> 
-            <p>
-              grgrviervmgervgmervmgerivmrev. gmirgvmgivmrivmrv
-              vmrivmrvmrvir.
-            </p>
+
+            {/* CARD TEXT */}
+            <div className="info">
+              <h5>Jericho Cruz</h5>
+              <q>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Suscipit libero mollitia nam saepe voluptatum modi quasi iste.
+                Sunt cupiditate tenetur non ipsa, ducimus eum aperiam atque
+                libero, praesentium sequi esse.
+              </q>
+            </div>
           </div>
-          <div class="testimonials-box item">
-            <div class="testimonial-photo">
-              <img src="/assets/test.jpg" alt="Client Photo"></img>
+
+          {/* BELOW IS REPITION OF THE ACTUAL TEXT IN THE TOP SAME STYLING AND CONTENT */}
+          <div className="card">
+            <div className="client-image">
+              <img src={ClientImage} alt="Client Photo" />
             </div>
-            <h5>George Luna</h5>
-            <p>
-              grgrviervmgervgmervmgerivmrev. gmirgvmgivmrivmrv
-              vmrivmrvmrvir.
-            </p>
+            <div className="info">
+              <h5>Jericho Cruz</h5>
+              <q>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Suscipit libero mollitia nam saepe voluptatum modi quasi iste.
+                Sunt cupiditate tenetur non ipsa, ducimus eum aperiam atque
+                libero, praesentium sequi esse.
+              </q>
+            </div>
           </div>
-          <div class="testimonials-box item"> 
-            <div class="testimonial-photo">
-              <img src="/assets/test.jpg" alt="Client Photo"></img>
+
+          <div className="card">
+            <div className="client-image">
+              <img src={ClientImage} alt="Client Photo" />
             </div>
-            <h5>Jonathan Cruz</h5>
-            <p>
-              grgrviervmgervgmervmgerivmrev. gmirgvmgivmrivmrv
-              vmrivmrvmrvir.
-            </p>
-          </div>  
+            <div className="info">
+              <h5>Jericho Cruz</h5>
+              <q>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Suscipit libero mollitia nam saepe voluptatum modi quasi iste.
+                Sunt cupiditate tenetur non ipsa, ducimus eum aperiam atque
+                libero, praesentium sequi esse.
+              </q>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </div> 
   )
 }
 
