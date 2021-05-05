42fa1167a96b1752f3b71f336cdb36b2a0d0a5ad
DarwinCamahalan <camahalandarwin@gmail.com>
2021-05-05 21:30:27 +0800

    Added Edit !Functionallity

--- src/redux/Products/products.reducer.js
+++ src/redux/Products/products.reducer.js
@@ -1,26 +1,39 @@
-import productsTypes from './products.types';
-import productTypes from './products.types';
+import productsTypes from './products.types'
+import productTypes from './products.types'
 
 const INITIAL_STATE = {
   products: [],
   product: {},
-};
+}
 
-const productsReducer = (state=INITIAL_STATE, action) => {
-  switch(action.type) {
+const productsReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
     case productTypes.SET_PRODUCTS:
       return {
         ...state,
-        products: action.payload
+        products: action.payload,
       }
     case productsTypes.SET_PRODUCT:
       return {
         ...state,
-        product: action.payload
+        product: action.payload,
       }
+    case productsTypes.EDIT_PRODUCT_SUCCESS: //EDIT
+      return {
+        ...state,
+        products: state.products.map((product) => {
+          if (product.productID === action.payload.productID) {
+            return {
+              ...product,
+              ...action.payload.update,
+            }
+          }
+          return product
+        }),
+      } //EDIT
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default productsReducer;
+export default productsReducer
