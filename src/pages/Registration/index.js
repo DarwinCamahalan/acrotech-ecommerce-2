ad24ed4b1b4893f9b31bd6a7ac74614b30ca3904
DarwinCamahalan <camahalandarwin@gmail.com>
2021-05-06 16:42:39 +0800

    Added Home Page

--- src/pages/Registration/index.js
+++ src/pages/Registration/index.js
@@ -4,23 +4,25 @@ import './styles.scss'
 import { Link } from 'react-router-dom'
 const Registration = (props) => {
   return (
-    <div className="signup">
-      <div className="info">
-        <h1>User Registration</h1>
-        <hr />
-        <div className="hint">Entry of customer information</div>
-        <p>
-          If you would like to register as a user, please be sure to enter all
-          the necessary items, and click the Register button.
-        </p>
-      </div>
+    <div className="max">
+      <div className="signup">
+        <div className="info">
+          <h1>User Registration</h1>
+          <hr />
+          <div className="hint">Entry of customer information</div>
+          <p>
+            If you would like to register as a user, please be sure to enter all
+            the necessary items, and click the Register button.
+          </p>
+        </div>
 
-      <div className="registration-container">
-        <Signup />
-        <div className="register">
-          <h1>Recover Account</h1>
+        <div className="registration-container">
+          <Signup />
+          <div className="register">
+            <h1>Recover Account</h1>
 
-          <Link to="/recovery">Forgot your password?</Link>
+            <Link to="/recovery">Forgot your password?</Link>
+          </div>
         </div>
       </div>
     </div>
