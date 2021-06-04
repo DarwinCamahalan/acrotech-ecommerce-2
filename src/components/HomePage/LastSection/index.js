c7cde4a952f1ed71c8833759bc350ba654078aa7
DarwinCamahalan <camahalandarwin@gmail.com>
2021-06-04 12:38:15 +0800

    Added Temp

--- src/components/HomePage/LastSection/index.js
+++ src/components/HomePage/LastSection/index.js
@@ -5,10 +5,13 @@ import 'aos/dist/aos.css'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
 
-import client1 from '../../../assets/client1.jpg'
-import client2 from '../../../assets/client2.jpg'
-import client3 from '../../../assets/client3.jpg'
+// import client1 from '../../../assets/client1.jpg'
+// import client2 from '../../../assets/client2.jpg'
+// import client3 from '../../../assets/client3.jpg'
 
+import temp1 from '../../../assets/temp1.jpg'
+import temp2 from '../../../assets/temp2.jpg'
+import temp3 from '../../../assets/temp3.jpg'
 const LastSection = ({}) => {
   useEffect(() => {
     AOS.init({
@@ -28,30 +31,52 @@ const LastSection = ({}) => {
   }
 
   const Cards = [
+    // {
+    //   img: `${client1}`,
+    //   name: 'Juan dela Cruz',
+    //   description:
+    //     "I like Acrotech Products, It's durable high quality and reliable at any situation also the design is so good and comforable to use",
+    // },
+    // {
+    //   img: `${client2}`,
+    //   name: 'Martin Luther',
+    //   description:
+    //     "I like Acrotech Products, It's durable high quality and reliable at any situation also the design is so good and comforable to use",
+    // },
+    // {
+    //   img: `${client3}`,
+    //   name: 'Kyle Joe',
+    //   description:
+    //     "I like Acrotech Products, It's durable high quality and reliable at any situation also the design is so good and comforable to use",
+    // },
     {
-      img: `${client1}`,
-      name: 'Juan dela Cruz',
+      img: `${temp3}`,
+      name: 'Justine Joy Acle',
+      position: 'Head Marketing',
       description:
-        "I like Acrotech Products, It's durable high quality and reliable at any situation also the design is so good and comforable to use",
+        'University of Science and Technology of Southern Philippines',
     },
     {
-      img: `${client2}`,
-      name: 'Martin Luther',
+      img: `${temp1}`,
+      name: 'Mary Jane Calaganda',
+      position: 'Human Resource Management',
       description:
-        "I like Acrotech Products, It's durable high quality and reliable at any situation also the design is so good and comforable to use",
+        'University of Science and Technology of Southern Philippines',
     },
     {
-      img: `${client3}`,
-      name: 'Kyle Joe',
+      img: `${temp2}`,
+      name: 'Princess Ranada',
+      position: 'Project Manager',
       description:
-        "I like Acrotech Products, It's durable high quality and reliable at any situation also the design is so good and comforable to use",
+        'University of Science and Technology of Southern Philippines',
     },
   ]
   return (
     <div className="max">
       <section className="last-section" data-aos="fade-up">
         <div className="title">
-          <h1>Client Review</h1>
+          {/* <h1>Client Review</h1> */}
+          <h1>Group Members</h1>
         </div>
 
         <Carousel
@@ -69,7 +94,7 @@ const LastSection = ({}) => {
                   <img src={Cards.img} alt={Cards.img} />
                 </div>
                 <h1>{Cards.name}</h1>
-
+                <h5>{Cards.position}</h5>
                 <p>{Cards.description}</p>
               </div>
             )
--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -1,27 +1,29 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import { firebaseConfig } from './config';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+import { firebaseConfig } from './config'
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+export const storage = firebase.storage()
 
-export const GoogleProvider = new firebase.auth.GoogleAuthProvider();
-GoogleProvider.setCustomParameters({ prompt: 'select_account' });
+export const GoogleProvider = new firebase.auth.GoogleAuthProvider()
+GoogleProvider.setCustomParameters({ prompt: 'select_account' })
 
 export const handleUserProfile = async ({ userAuth, additionalData }) => {
-  if (!userAuth) return;
-  const { uid } = userAuth;
+  if (!userAuth) return
+  const { uid } = userAuth
 
-  const userRef = firestore.doc(`users/${uid}`);
-  const snapshot = await userRef.get();
+  const userRef = firestore.doc(`users/${uid}`)
+  const snapshot = await userRef.get()
 
   if (!snapshot.exists) {
-    const { displayName, email } = userAuth;
-    const timestamp = new Date();
-    const userRoles = ['user'];
+    const { displayName, email } = userAuth
+    const timestamp = new Date()
+    const userRoles = ['user']
 
     try {
       await userRef.set({
@@ -29,21 +31,21 @@ export const handleUserProfile = async ({ userAuth, additionalData }) => {
         email,
         createdDate: timestamp,
         userRoles,
-        ...additionalData
-      });
-    } catch(err) {
+        ...additionalData,
+      })
+    } catch (err) {
       // console.log(err);
     }
   }
 
-  return userRef;
-};
+  return userRef
+}
 
 export const getCurrentUser = () => {
   return new Promise((resolve, reject) => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
-      unsubscribe();
-      resolve(userAuth);
-    }, reject);
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      unsubscribe()
+      resolve(userAuth)
+    }, reject)
   })
 }
